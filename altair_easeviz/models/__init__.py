e54238d7ad7765c16ed20ed7158c0211b20d0416
Miguel Huayllas <Mhuaylch10@alumnes.ub.edu>
2024-01-11 00:17:39 +0100

    First commit
    Added all cores files from previous repository, this change was made to change the name of the pacakge

---
+++ altair_easeviz/models/__init__.py
@@ -0,0 +1,2 @@
+from altair_easeviz.models.models_theme import AccessibleTheme, DarkAccessibleTheme, FillerPatternTheme, PrintFriendlyTheme, ModelTheme
+from altair_easeviz.models.models_config import ConfigModel
